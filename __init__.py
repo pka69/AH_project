8a03261ba7e362036e80df04db7a5923d9990c9f
Piotr Kalista <pjkalista@gmail.com>
2022-05-12 08:54:29 +0200

    added __version__  information

--- __init__.py
+++ __init__.py
@@ -2,6 +2,8 @@ wraper = None
 status_df = None
 debug_level = 0
 
+__version__ = '1.005'
+__date__ = "2022/05/12"
 def set_init(wraper, status_df, debug_level):
     wraper = wraper
     status_df = status_df
